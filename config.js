da1e0413d4addbd7a5674033a97152c4bbbfd932
UNKNOWN-SER <102516170+UNKNOWN-SER@users.noreply.github.com>
2022-03-31 21:12:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['918113921898'] // Put your number here
+global.owner = ['917561823024'] // Put your number here
 global.mods =  JSON.parse(fs.readFileSync('./src/moderator.json'))// Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium users do not need a limit
 global.tod = JSON.parse(fs.readFileSync('./src/tod.json'))
@@ -55,7 +55,7 @@ global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://github.com/neera-j/Bot-Files/blob/main/images/millie.jpg'
 
-global.watermark = '© ᴍɪʟʟɪᴇ'
+global.watermark = '© ɪͥᴛͭsᷤ͢ᴍͫᴇͤᴏɢɢʏ-ꜱᴇʀ'
 
 global.multiplier = 69 // The higher it is, the harder it is to level up
 
